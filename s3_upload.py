9f1648201c0cde2ec8133f3ff540b10195f263d1
Yael Lyshkow <yaellyshkow@outlook.com>
2024-09-21 13:22:21 -0400

    added function

--- s3_upload.py
+++ s3_upload.py
@@ -13,6 +13,21 @@ def check_file_exists(bucket_name, object_name):
         else:
             raise
 
+def get_s3_image_links(bucket_name: str, folder_name: str):
+    s3 = boto3.client('s3')
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
+
+    bucket_location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
+    base_url = f"https://{bucket_name}.s3.{bucket_location}.amazonaws.com"
+    image_links = []
+
+    for obj in response.get('Contents', []):
+        object_url = f"{base_url}/{obj['Key']}"
+        
+        if obj['Key'].endswith(('.jpg', '.jpeg', '.png', '.gif')):
+            image_links.append(object_url)
+
+    return image_links
 
 def upload_image_to_s3(file_name, bucket_name, object_name=None):
 
